e9092da68ff960bda9554cc70db4d97996019a85
greenHound <rv.rishabhverma1996@gmail.com>
2021-09-15 23:51:09 +0530

    Added actions

--- components/RichTextEditor.component.js
+++ components/RichTextEditor.component.js
@@ -1,7 +1,7 @@
 import  React, { createRef, useState, useEffect } from 'react';
 import Styles from '../styles/RichText.module.css';
 import { BsTypeBold, BsTypeItalic, BsJustifyLeft, BsJustify, BsJustifyRight, BsTextCenter, BsCodeSlash, BsTypeUnderline } from 'react-icons/bs';
-import { BiHeading } from 'react-icons/bi';
+import { IoMdArrowBack } from 'react-icons/io';
 import { AiOutlineOrderedList, AiOutlineUnorderedList, AiOutlineLink } from 'react-icons/ai';
 
 const RichTextEditor = () => {
@@ -23,6 +23,9 @@ const RichTextEditor = () => {
     const handleClick = (command) => {
         focusRef.current.contentDocument.execCommand(command)
     }
+
+
+    
     
 
     const toggleCode = () => {
@@ -37,13 +40,31 @@ const RichTextEditor = () => {
     }
 
     const handleSendData = () => {
-        window.ReactNativeWebView.postMessage(JSON.stringify({name:"Rishabh Verma"}));
+        const textBody = focusRef.current.contentDocument.querySelector('body');
+        window.ReactNativeWebView.postMessage(JSON.stringify({content:textBody.innerHTML}));
+    }
+
+
+    const handleBackPress = () => {
+        window.ReactNativeWebView.postMessage(JSON.stringify({action:"GoBack"}));
     }
 
 
 
     return(
         <div className={Styles.container}>
+            <div className={Styles.actionBar}>
+                <div className={Styles.actionContainer} onClick={handleBackPress} >
+                    <div className={Styles.backIcon}>
+                        <IoMdArrowBack size={24} color="black"  />
+                    </div>
+                    <p className={Styles.sectionTitle}>Story</p>
+                </div>
+
+                <div className={Styles.primaryAction} onClick={handleSendData} >
+                    <p className={Styles.primaryActionLabel}>Save Story</p>
+                </div>
+            </div>
             <div className={Styles.editorContainer}>
                 <div className={Styles.editorControls}>
                     <BsTypeBold onClick={() => handleClick("bold")}  size={24} color='#444' className={Styles.editorControl} />
