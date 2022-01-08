a71d54aced8058ef4b09d78093a4f82b56edfe43
greenHound <rv.rishabhverma1996@gmail.com>
2022-01-09 02:48:45 +0530

    trial

--- components/RichTextEditor.component.js
+++ components/RichTextEditor.component.js
@@ -73,7 +73,7 @@ const RichTextEditor = () => {
     const handleBackPress = () => {
         const textBody = focusRef.current.contentDocument.querySelector('body');
         window.ReactNativeWebView.postMessage(JSON.stringify({action:"GoBack", content:textBody.innerHTML}));
-        window.top.postMessage('hello', '*')
+        window.parent.postMessage('hello', '*')
     }
 
     const handleYoutube = (videoId) => {
